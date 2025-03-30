f71283a81befa7d7921dc73292eba9d21461c179
Karenop4 <karenorpe2004@gmail.com>
2025-03-29 23:30:03 -0500

    Actualización en la programación de los dados
    - Se aplicó la animación de la rotación de los dados
    - Se vinculó el puntaje con los resultados que arrojan los dados
    - Se cambió el color para identificar el dado del jugador 1 y del 2

--- duelo-dados.js
+++ duelo-dados.js
@@ -3,13 +3,16 @@ let boton2 = document.querySelector(".BttonJ2")
 let botonR = document.querySelector(".Reiniciar button")
 let rondas = document.querySelector(".Rondas #RonTur")
 let mp=document.querySelector(".Rondas #MP")
+const dado1 = document.querySelector(".dado")
+const dado2 = document.querySelector(".dado2")
 let ronda = 1;
 let turno =1;
 let ganador="";
-let puntuacionJ1=1;
+let puntuacionJ1=0;
 let puntuacionJ2=0;
 const MejorPuntuacion=localStorage.getItem('MejorPuntuacion');
 let audioEtiqueta = document.querySelector("audio")
+const time = 2;
 boton2.disabled=true;
 actualizarRonda();
 
@@ -18,22 +21,104 @@ actualizarRonda();
 //lanzar los dados y se habilita el boton contrario, ademas, reproduce un sonido de dados
 boton.addEventListener("click", () => {
     audioEtiqueta.setAttribute("src", "audio/dados.mp3")
-    audioEtiqueta.play()
     boton.disabled=true;
     boton2.disabled=false;
     turno=turno+1;
-    actualizarRonda();
+
+
+    dado1.style.transition = '';
+    dado1.style.transform = `translateY(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+
+    setTimeout(() => {
+        dado1.style.transition = `transform 0.5s`;
+        dado1.style.transform = `translateY(-50px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+
+        setTimeout(() => {
+            audioEtiqueta.play()
+            setTimeout(() => {
+                dado1.style.transition = `transform ${time}s`;
+                let randomValue = Math.floor((Math.random() * 6) + 1);
+                console.log(`randomValue: ${randomValue}`);
+
+                puntuacionJ1+=randomValue;
+                console.log(`puntuacionJ1: ${puntuacionJ1}`);
+                switch (randomValue) {
+                    case 1:
+                        dado1.style.transform = `translateY(50px) rotateX(3600deg) rotateY(3600deg) rotateZ(3600deg)`;
+                        break;
+                    case 2:
+                        dado1.style.transform = `translateY(50px) rotateX(4410deg) rotateY(3600deg) rotateZ(3600deg)`;
+                        break;
+                    case 3:
+                        dado1.style.transform = `translateY(50px) rotateX(3600deg) rotateY(4410deg) rotateZ(3600deg)`;
+                        break;
+                    case 4:
+                        dado1.style.transform = `translateY(50px) rotateX(3600deg) rotateY(2430deg) rotateZ(3600deg)`;
+                        break;
+                    case 5:
+                        dado1.style.transform = `translateY(50px) rotateX(2430deg) rotateY(3600deg) rotateZ(3600deg)`;
+                        break;
+                    case 6:
+                        dado1.style.transform = `translateY(50px) rotateX(3600deg) rotateY(1980deg) rotateZ(3600deg)`;
+                        break;
+                };
+                actualizarRonda();
+            }, 80);
+        }, 100); 
+        
+    }, 100);
+
 })
+
 boton2.addEventListener("click", () => {
     audioEtiqueta.setAttribute("src", "audio/dados.mp3")
-    audioEtiqueta.play()
     boton.disabled=false;
     boton2.disabled=true;
     turno=1;
     ronda=ronda+1;
-    actualizarRonda();
-})
 
+    dado2.style.transition = '';
+    dado2.style.transform = `translateY(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+
+    setTimeout(() => {
+        dado2.style.transition = `transform 0.5s`;
+        dado2.style.transform = `translateY(-50px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+
+        setTimeout(() => {
+            audioEtiqueta.play()
+            setTimeout(() => {
+                dado2.style.transition = `transform ${time}s`;
+                let randomValue = Math.floor((Math.random() * 6) + 1);
+                console.log(`randomValue: ${randomValue}`);
+                puntuacionJ2+=randomValue;
+                console.log(`puntuacionJ2: ${puntuacionJ2}`);
+                switch (randomValue) {
+                    case 1:
+                        dado2.style.transform = `translateY(50px) rotateX(3600deg) rotateY(3600deg) rotateZ(3600deg)`;
+                        break;
+                    case 2:
+                        dado2.style.transform = `translateY(50px) rotateX(4410deg) rotateY(3600deg) rotateZ(3600deg)`;
+                        break;
+                    case 3:
+                        dado2.style.transform = `translateY(50px) rotateX(3600deg) rotateY(4410deg) rotateZ(3600deg)`;
+                        break;
+                    case 4:
+                        dado2.style.transform = `translateY(50px) rotateX(3600deg) rotateY(2430deg) rotateZ(3600deg)`;
+                        break;
+                    case 5:
+                        dado2.style.transform = `translateY(50px) rotateX(2430deg) rotateY(3600deg) rotateZ(3600deg)`;
+                        break;
+                    case 6:
+                        dado2.style.transform = `translateY(50px) rotateX(3600deg) rotateY(1980deg) rotateZ(3600deg)`;
+                        break;
+                };
+                actualizarRonda();
+            }, 80);
+        }, 100); 
+        
+    }, 100);
+
+})
 
 //Listener para boton que reinicia la pagina 
 botonR.addEventListener("click", ()=>{
