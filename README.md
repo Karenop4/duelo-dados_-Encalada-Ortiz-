ebb0898b1c410a00a6a9e02237f360a1eb73345e
Andrés Encalada <encaladaandres15@gmail.com>
2025-03-29 19:52:42 -0500

    Actualización de Botones
    Se actualizan los botones para que se deshabiliten según el turno actual, ahora se muestra el ganador cuando se finalizan las 3 rondas y este resultado (en caso de romper el record) se guarda en localStorage.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-README
 El proyecto presenta un pequeño juego en el que los jugadores se turnan para lanzar un par de dados,
 después de cada lanzamiento su puntuación se registra en un marcador. Después de 3 turnos el jugador
 con la mayor puntuación gana y se presenta en pantalla.
--- duelo-dados.js
+++ duelo-dados.js
@@ -1,12 +1,66 @@
 let boton = document.querySelector(".BttonJ1")
 let boton2 = document.querySelector(".BttonJ2")
+let botonR = document.querySelector(".Reiniciar button")
+let rondas = document.querySelector(".Rondas #RonTur")
+let mp=document.querySelector(".Rondas #MP")
+let ronda = 1;
+let turno =1;
+let ganador="";
+let puntuacionJ1=1;
+let puntuacionJ2=0;
+const MejorPuntuacion=localStorage.getItem('MejorPuntuacion');
 let audioEtiqueta = document.querySelector("audio")
+boton2.disabled=true;
+actualizarRonda();
 
+
+//Listener especifico para cada boton, en ambos se deshabilita el propio boton despues de
+//lanzar los dados y se habilita el boton contrario, ademas, reproduce un sonido de dados
 boton.addEventListener("click", () => {
     audioEtiqueta.setAttribute("src", "audio/dados.mp3")
     audioEtiqueta.play()
+    boton.disabled=true;
+    boton2.disabled=false;
+    turno=turno+1;
+    actualizarRonda();
 })
 boton2.addEventListener("click", () => {
     audioEtiqueta.setAttribute("src", "audio/dados.mp3")
     audioEtiqueta.play()
+    boton.disabled=false;
+    boton2.disabled=true;
+    turno=1;
+    ronda=ronda+1;
+    actualizarRonda();
 })
+
+
+//Listener para boton que reinicia la pagina 
+botonR.addEventListener("click", ()=>{
+    location.reload(true);
+})
+//Mostramos la mejor puntuación registrada del localStorage
+if(MejorPuntuacion!= null){
+    mp.innerText="\nMejor Puntuación: "+MejorPuntuacion;
+}
+//Actualizamos el texto de turnos y rondas, esta funcion es llamada por cada boton despues de un lanzamiento
+//Una vez finalicen las 3 rondas el juego termina
+function actualizarRonda(){
+    rondas.innerText="Ronda: "+ronda+"\n  Turno: Jugador "+turno;
+    if (ronda==4) {
+        if (puntuacionJ1>puntuacionJ2) {
+            ganador="Jugador 1";
+            localStorage.setItem('MejorPuntuacion',puntuacionJ1);
+        }
+        else if(puntuacionJ1==puntuacionJ2){
+            ganador="Empate";
+        }
+        else{
+            ganador="Jugador 2";
+            localStorage.setItem('MejorPuntuacion',puntuacionJ2);
+        }
+        rondas.innerText="FIN DEL JUEGO!!! \n Ganador:"+ganador;
+        boton.disabled=true;
+        boton2.disabled=true;
+    }
+}
